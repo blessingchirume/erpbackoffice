c596bd1a9c8104fceec5bae1e7db201a281fe455
blessing-chirume <chirume37@gmail.com>
2024-10-22 17:08:57 +0200

    Restructured reciept response

--- app/Http/Controllers/Api/ReceiptController.php
+++ app/Http/Controllers/Api/ReceiptController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Receipt;
 use Illuminate\Http\Request;
 
 class ReceiptController extends Controller
 {
-    //
+   public function index(){
+    
+    $receipts = Receipt::all()->map(function ($receipt) {
+        return [
+            "id" => $receipt->id,
+            "title" => $receipt->title,
+            "provider_id" => $receipt->provider->name ?? '',
+            "user_id" => $receipt->user->name,
+            "total" => $receipt->products->sum('stock'),
+            "products" => $receipt->products->map(function($product){
+                return [                
+                    "product_id" => $product->id,
+                    "product_name" => $product->product->name,
+                    "stock" => $product->stock,
+                    "stock_defective" => $product->stock_defective,
+                ];
+            })
+        ];
+    });
+
+    return response($receipts);
+   }
 }
