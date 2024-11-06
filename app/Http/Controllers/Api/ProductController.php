32fd1af945946ea2b03764c706fd9c2584b91f03
blessing-chirume <chirume37@gmail.com>
2024-11-06 14:57:42 +0200

    categories get endpoint

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -10,11 +10,6 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $products = Product::all();
@@ -22,25 +17,6 @@ class ProductController extends Controller
         return response($products);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $categories = ProductCategory::all();
-
-        return view('inventory.products.create', compact('categories'));
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  App\Http\Requests\ProductRequest  $request
-     * @param  App\Product  $model
-     * @return \Illuminate\Http\Response
-     */
     public function store(ProductRequest $request, Product $model)
     {
         $product = Product::create($request->all());
@@ -48,12 +24,6 @@ class ProductController extends Controller
         return response("Product created successfully", 200);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(Product $product)
     {
         $solds = $product->solds()->latest()->limit(25)->get();
@@ -63,12 +33,6 @@ class ProductController extends Controller
         return view('inventory.products.show', compact('product', 'solds', 'receiveds'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Product $product)
     {
         $categories = ProductCategory::all();
@@ -76,13 +40,6 @@ class ProductController extends Controller
         return view('inventory.products.edit', compact('product', 'categories'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  Product  $product
-     * @return \Illuminate\Http\Response
-     */
     public function update(ProductRequest $request, Product $product)
     {
         $product->update($request->all());
@@ -92,12 +49,6 @@ class ProductController extends Controller
             ->withStatus('Product updated successfully.');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  Product  $product
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Product $product)
     {
         $product->delete();
