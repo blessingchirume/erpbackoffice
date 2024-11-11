ebef90d4b84c8609411d07e46ce6a11ef2f312c0
blessing-chirume <chirume37@gmail.com>
2024-11-11 14:25:55 +0200

    Added company registration on mobile

---
+++ app/Http/Controllers/ApiCompanyController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Constants\ApplicationRoleConstants;
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class ApiCompanyController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'company_name' => 'required',
+            'business_type' => 'required',
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+
+        ]);
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+        ]);
+
+        $user->assignRole(ApplicationRoleConstants::SystemAdmin);
+
+        $tenant = Company::create([
+            'name' => $request->get('company_name'),
+            'email' => $request->get('email'),
+            'business_type' => $request->get('business_type'),
+            'company_db_name' => str_replace(' ', '_', $request->get('company_name')),
+        ]);
+
+        $user->company_id = $tenant->id;
+
+        $user->save();
+
+        $tenant->createDatabase($tenant->company_db_name);
+
+        return response('Successfully registered tenant.\n
+         You can visit retailmate.co.zw to start adding your users', 200);
+    }
+
+}
