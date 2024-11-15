09668a6c35817d70f706e02e6352408aa8d2f3ea
blessing-chirume <chirume37@gmail.com>
2024-11-15 16:15:49 +0200

    Commented out global seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            PermissionSeeder::class,
-            RoleSeeder::class,
-            UserSeeder::class,
+//            PermissionSeeder::class,
+//            RoleSeeder::class,
+//            UserSeeder::class,
             DefaultDataSeeder::class
 //            CurrencySeeder::class
         ]);
